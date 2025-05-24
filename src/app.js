6b248c96febc26f85e6910c60265d3fee9b209df
sunay kundalwal <sunaykundalwal@outlook.in>
2025-05-24 19:02:07 +0530

    9th commit

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const app = express()
 
 app.use(
     cors({
-        origin:["http://127.0.0.1:3004", "http://localhost:3004","https://creatortube-production.up.railway.app"],
+        origin:["http://127.0.0.1:3004", "http://localhost:3004","https://creator-tube-phi.vercel.app"],
         credentials: true 
     })
 )
