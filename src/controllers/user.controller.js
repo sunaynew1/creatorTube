45afee2c53820ad425f097717f1cad27f301b12b
sunay kundalwal <sunaykundalwal@outlook.in>
2025-06-03 09:59:11 +0530

    commit new 110

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -996,12 +996,12 @@ const saveVideo = asyncHandler(async(req,res) => {
 })
 
 const history =asyncHandler(async(req,res) => {
-    console.log("history")
-    return res.status(200).json(new ApiResponse(200,"history reached"))
-    // const token = req.cookies.accessToken
-    // const user = await User.findOne({accessToken : token})
-    // const watchHistory = user.select("watchHistory")
-    // return res.status(200).json(new ApiResponse(200,watchHistory))
+    // console.log("history")
+    // return res.status(200).json(new ApiResponse(200,"history reached"))
+    const token = req.cookies.accessToken
+    const user = await User.findOne({accessToken : token})
+    const watchHistory = user.select("watchHistory")
+    return res.status(200).json(new ApiResponse(200,watchHistory))
 })
 
 export {
