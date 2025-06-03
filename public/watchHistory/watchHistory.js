7ca71bf72921dcdd93fb1f6150b3db99790cc0a5
sunay kundalwal <sunaykundalwal@outlook.in>
2025-06-03 13:38:37 +0530

    commit new 143

--- public/watchHistory/watchHistory.js
+++ public/watchHistory/watchHistory.js
@@ -19,10 +19,11 @@ async function watchHistory() {
 console.log("reached here watch history")
 
     async function videoCard (){
-        const data=  await watchHistory()
+        const response=  await watchHistory()
+        const data = response.data
         console.log(`watch history data frontend s: ${data}`)
-    data.forEach(videoId => {
-        
+    data.forEach(v => {
+        const videoId = v.videoId 
         const card = document.createElement("div");
         card.className = "rounded-2xl overflow-hidden bg-white/5 border border-white/10 backdrop-blur-lg shadow-lg hover:ring-1 hover:ring-purple-500 transition-all";
 
