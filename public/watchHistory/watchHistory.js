142e2fcf79649aff08fb3626a42d8f0514f169dc
sunay kundalwal <sunaykundalwal@outlook.in>
2025-06-03 13:06:02 +0530

    commit new 136

--- public/watchHistory/watchHistory.js
+++ public/watchHistory/watchHistory.js
@@ -21,7 +21,7 @@ console.log("reached here watch history")
 async function videoCard (){
     const data=  await watchHistory()
    
-data.forEach(v => {
+data.data.watchHistory.forEach(v => {
      const video = v.videoId
       const card = document.createElement("div");
       card.className = "rounded-2xl overflow-hidden bg-white/5 border border-white/10 backdrop-blur-lg shadow-lg hover:ring-1 hover:ring-purple-500 transition-all";
