527247e74bfbd8d3868b6eac8a9a194b713080f6
sunay kundalwal <sunaykundalwal@outlook.in>
2025-06-04 19:10:15 +0530

    commit new 204

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -66,7 +66,7 @@ const userSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "Video",
         },
-        timestamps:true
+        visitedAt: new Date()
     
 }],
     saved: [{
